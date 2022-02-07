e0c489bfda8577c91a81f4ef547c1d970f53727c
Chinmai Sai Eshwar Reddy Kasi <chinmai.kasi55@gmail.com>
2022-02-07 16:36:42 -0500

    Assignment 1 changes

--- Project 1/P1/DBFile.cc
+++ Project 1/P1/DBFile.cc
@@ -36,15 +36,22 @@ void DBFile::Load (Schema &f_schema, const char *loadpath) {
             dataFile.AddPage(&page, pageNumber);
         }
     } else {
-        cout << "Error occurent when opening file.";
+        cout << "Error occured while opening the file.";
     }
     fclose(textFile);
 }
 
 int DBFile::Open (const char *f_path) {
+    dataFile.Open(1, (char *) f_path);
+    stillWriting = false;
+    pageNumber = 0;
+    return 1;
 }
 
 void DBFile::MoveFirst () {
+    if(dataFile.GetLength() > 0){
+        dataFile.GetPage(&page, 0);
+    }
 }
 
 int DBFile::Close () {
@@ -74,7 +81,26 @@ void DBFile::Add (Record &rec) {
 }
 
 int DBFile::GetNext (Record &fetchme) {
+    if(stillWriting){ MoveFirst();}
+
+    if(page.GetFirst(&fetchme) == 0){
+        pageNumber += 1;
+        if (pageNumber >= dataFile.GetLength() - 1){
+            return 0;
+        } else {
+            page.EmptyItOut();
+            dataFile.GetPage(&page, pageNumber);
+            page.GetFirst(&fetchme);
+        }
+    }
+    return 1;
 }
 
 int DBFile::GetNext (Record &fetchme, CNF &cnf, Record &literal) {
+    ComparisonEngine ce;
+
+    while(GetNext(fetchme)==1){
+        if(ce.Compare(&fetchme, &literal, &cnf) == 1){return 1;}
+    }
+    return 0;
 }
