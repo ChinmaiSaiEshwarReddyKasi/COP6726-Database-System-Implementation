a45fa0bccef74a4a17b4ab9e7a1d5e8f41c3b5b3
Chinmai Sai Eshwar Reddy Kasi <chinmai.kasi55@gmail.com>
2022-02-28 19:32:22 -0500

    test cases

---
+++ Project 2/P1/gTests.cc
@@ -0,0 +1,108 @@
+#include <gtest/gtest.h>
+#include <iostream>
+#include "Record.h"
+#include "DBFile.h"
+#include "BigQ.h"
+#include <stdlib.h>
+
+char *nationFile = "db/nation.bin";
+char *tempFile ="db/test.bin";
+
+
+using namespace std;
+File f;
+Page page;
+int idx = 0;
+DBFile dataFile;
+ComparisonEngine ce;
+Schema* s = new Schema("catalog", "nation");
+OrderMaker* o = new OrderMaker(s);
+
+TEST(BigQTest, TestSortingBufferRecords) {
+    f.Open(1, nationFile);
+    priority_queue<Record*, vector<Record*>, CompareRecords> bufferQ (o);
+    f.GetPage(&page, 0);
+    Record* r = new Record;
+    while (page.GetFirst(r)) {
+        bufferQ.push(r);
+        r = new Record;
+    }
+
+    File tmp;
+    tmp.Open(0, nationFile);
+    Page tmpPage;
+    priority_queue<class TPMMS*, vector<class TPMMS*>, CompareBuffers> tmpBufferQ (o);
+    tpmmsPipeline(bufferQ, tmpBufferQ, f, page, idx);
+    EXPECT_EQ(bufferQ.size(), 0);
+    EXPECT_EQ(tmpBufferQ.size(), 1);
+    f.Close();
+}
+
+TEST(BigQTest, TestUpdatingFirstRecord) {
+    Record r;
+    f.Open(1, nationFile);
+    class TPMMS* tpmms = new class TPMMS(&f, 0, 1);
+    f.GetPage(&page, 0);
+    page.GetFirst(&r);
+    while (page.GetFirst(&r)) {
+        EXPECT_EQ(tpmms->firstRecordUpdate(), 1);
+    }
+    f.Close();
+}
+
+TEST(BigQTest, TestSortingRecords) {
+    f.Open(1, nationFile);
+    priority_queue<Record*, vector<Record*>, CompareRecords> recQ (o); 
+
+    f.GetPage(&page, 0);
+    Record* r = new Record;
+    while (page.GetFirst(r)) {
+        recQ.push(r);
+        r = new Record;
+    }
+
+    page.EmptyItOut();
+    f.GetPage(&page, 0);
+    Record tmpRec[2];
+    Record *end = NULL, *before = NULL;
+    int i = 0;
+    while (page.GetFirst(&tmpRec[i%2]) == 1) {
+        before = end;
+        end = &tmpRec[i%2];
+        if (before && end) {
+            EXPECT_EQ(ce.Compare(before, end, o), -1);
+        }
+        i++;
+    }
+    f.Close();
+}
+
+TEST(BigQTest, RunComparerTest) {
+    //Initiate the proority queue for recard comparer
+    File file;
+    file.Open(1, "db/lineitem.bin");
+    Schema* scheme = new Schema("catalog", "lineitem");
+    OrderMaker* order = new OrderMaker(scheme);
+    priority_queue<class TPMMS*, vector<class TPMMS*>, CompareBuffers> runQueue (order);
+    ComparisonEngine comparisonEngine;
+
+    class TPMMS* run1 = new class TPMMS(&file, 0, 1);
+    class TPMMS* run2 = new class TPMMS(&file, 1, 1);
+    runQueue.push(run1);
+    runQueue.push(run2);
+
+    //Take top record from two different run
+    Record one, two;
+    one.Copy(runQueue.top()->firstRecord);
+    runQueue.pop();
+    two.Copy(runQueue.top()->firstRecord);
+    runQueue.pop();
+    EXPECT_EQ(comparisonEngine.Compare(&one, &two, order), -1);
+
+    file.Close();
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
